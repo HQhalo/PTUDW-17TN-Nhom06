ce5ed18bc2a7f1d02ce45a2d15f299af84726317
1712701 <haoquangms@gmail.com>
2020-05-22 14:59:07 +0700

    Add ThuThu_search

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  
 });
-
+router.get('/thuthu',function(req,res,next){
+  res.render("thuThu_search",{layout: "layout"});
+});
 module.exports = router;
